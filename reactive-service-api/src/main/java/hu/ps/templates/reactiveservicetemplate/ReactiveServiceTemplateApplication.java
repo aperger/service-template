3a7dafa4a61057fa7dcce8720f621034e50b6a48
Perger, Attila (BUDIT) - KLM <Atilla.Perger@klm.com>
2023-02-23 21:46:47 +0100

    Add new project called reactive-service-api

---
+++ reactive-service-api/src/main/java/hu/ps/templates/reactiveservicetemplate/ReactiveServiceTemplateApplication.java
@@ -0,0 +1,13 @@
+package hu.ps.templates.reactiveservicetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveServiceTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveServiceTemplateApplication.class, args);
+	}
+
+}
---
+++ reactive-service-api/src/test/java/hu/ps/templates/reactiveservicetemplate/ReactiveServiceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.ps.templates.reactiveservicetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveServiceTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
